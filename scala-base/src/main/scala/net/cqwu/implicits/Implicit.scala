548c86716c638e635eb381cb654f69500782a629
chenxuemin <cxm@iuicity.com>
2017-12-14 15:01:06 +0800

    隐式转换-方法

---
+++ scala-base/src/main/scala/net/cqwu/implicits/Implicit.scala
@@ -0,0 +1,30 @@
+package net.cqwu.implicits
+
+import java.util.{Calendar, Date}
+
+object Implicit {
+  //定义一个隐式转换方法
+  implicit def converInt2DateHelper(num: Int) = new DateHelper(num)
+
+    def main(args: Array[String]): Unit = {
+      var  past = 2 days("ago")
+      println(past)
+    }
+
+
+
+
+}
+
+//隐式转换方法
+class DateHelper(num: Int) {
+  def days(when: String): Date = {
+     var date = Calendar.getInstance
+    when match {
+      case "ago" => date.add(Calendar.DAY_OF_MONTH,-num)
+      case "from_now" => date.add(Calendar.DAY_OF_MONTH,num)
+      case _ => date
+    }
+    date.getTime
+  }
+}
